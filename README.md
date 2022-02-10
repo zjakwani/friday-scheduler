76d757fe773b15062bbdcf6380bffe8afd358431
Zain Jakwani <zjakwani@gmail.com>
2022-02-10 15:09:59 -0700

    finished script and formatting

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
 # friday-scheduler
 script to schedule Friday volunteering at ICC
+
+Instructions:
+Run the python file file to produce the schedule, You can rerun a few times and may get different outcome
+
+jummah_records.txt and jummah_settings.txt should be in the same folder as this file
+
+jummah_settings.txt has the names of people and timings they can volunteer for
+Each line formatted like this: Name:1,2,3
+
+jummah_records has list of past records, must be nonempty to produce schedule
+Each line formatted like this: Date:Names for 1st(comma separated)|Names for 2nd|Names for 3rd
---
+++ scheduler.py
@@ -0,0 +1,51 @@
+
+import os
+import sys
+from heapq import *
+
+settings = {}
+with open(os.path.join(sys.path[0], "jummah_settings.txt"), "r") as f:
+    for line in f:
+        name, times = line.split(":")
+        times = [int(x.strip()) - 1 for x in times.split(",")]
+        settings[name] = times
+# Zain Jakwani 02/04/22
+recordfile = open(os.path.join(sys.path[0], "jummah_records.txt"), "r")
+records = recordfile.readlines()
+mru = []
+for _line in reversed(records):
+    line2 = _line.split(":")
+    line3 = line2[1] if len(line2) > 1 else line2[0]
+    line = line3.replace("|", ",")
+    week_names = list(set([x.strip() for x in line.split(",")]))
+    for name in week_names:
+        if name not in mru:
+            mru.append(name)
+
+capacity = [3,3,1]
+res = [[],[],[]]
+cur = ["", [-1]]
+while len(res[0]) < capacity[0] or len(res[1]) < capacity[1] or len(res[2]) < capacity[2]:
+    if len(cur[1]) == 1:
+        popped = mru.pop()
+        cur = [popped, settings[popped]]
+    name, available = cur
+    if len(res[0]) < capacity[0] and 0 in available:
+        res[0].append(name)
+        cur[1].remove(0)
+    elif len(res[1]) < capacity[1] and 1 in available:
+        res[1].append(name)
+        cur[1].remove(1)
+    elif len(res[2]) < capacity[2] and 2 in available:
+        res[2].append(name)
+        cur[1].remove(2)
+    else:
+        popped = mru.pop()
+        cur = [popped, settings[popped]]
+
+print("Remember to update jummah_records.txt with all past weekslike this:")
+print("(Put date here):" + "|".join([(",").join(x) for x in res]) + "\n")
+print("This week's schedule")
+print("1st Jummah: " + ", ".join(res[0]))
+print("2nd Jummah: " + ", ".join(res[1]))
+print("3rd Jummah: " + ", ".join(res[2]))
